47bd050426636f0502a5f32e9850d6ac149c6b45
owen <owenmead@gmail.com>
2009-07-05 14:55:34 -0700

    Adds django project

---
+++ app/menu_items/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.name
+
+class MenuItem(models.Model):
+	name = models.CharField(max_length=200)
+	category = models.ForeignKey(Category)
+
+	def __unicode__(self):
+		return self.name
