8edcb2df9e4d6b90232707bdff5c8a3951653dd8
owen <owenmead@gmail.com>
2009-07-05 16:45:12 -0700

    Changes the admin view a bit

--- app/menu_items/admin.py
+++ app/menu_items/admin.py
@@ -1,5 +1,16 @@
 from app.menu_items.models import Category, MenuItem
 from django.contrib import admin
 
-admin.site.register(Category)
+class MenuItemInline(admin.TabularInline):
+	model = MenuItem
+	extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+	fieldsets = [
+		(None,               {'fields': ['name']}),
+	]
+	inlines = [MenuItemInline]
+
 admin.site.register(MenuItem)
+admin.site.register(Category, CategoryAdmin)
