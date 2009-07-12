aad11008b7bfb8cb03f68db490b9a6fcb7568a77
owen <owenmead@gmail.com>
2009-07-11 19:46:29 -0700

    Changes JSON output to a hand-made simpleJSON style

--- app/menu_items/views.py
+++ app/menu_items/views.py
@@ -1,14 +1,19 @@
 from app.menu_items.models import Category, MenuItem
 from django.http import HttpResponse
-from django.core import serializers
+#from django.core import serializers   -- little bit overkill
+from django.utils import simplejson
 
 def index(request):
 	return HttpResponse("INDEX")
 
 def categoryList(request):
-	data = serializers.serialize("json", Category.objects.all(), ensure_ascii=False)
-	return HttpResponse(data)
+	cats = []
+	for cat in Category.get_items():
+		cats.append({'id': cat.id, 'name': cat.name})
+	return HttpResponse(simplejson.dumps(cats))
 
 def menuitemList(request):
-	data = serializers.serialize("json", MenuItem.objects.all(), ensure_ascii=False)
-	return HttpResponse(data)
+	menu_items = []
+	for item in MenuItem.get_items():
+		menu_items.append({'id': item.id, 'name': item.name, 'categoryID': item.category.id})
+	return HttpResponse(simplejson.dumps(menu_items))
