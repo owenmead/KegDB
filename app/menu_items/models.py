815d485ff160adeaf416728dee09734c1261cc63
owen <owenmead@gmail.com>
2009-07-29 19:35:23 -0700

    Updates the model to include more on menu items

--- app/menu_items/models.py
+++ app/menu_items/models.py
@@ -15,6 +15,9 @@ class MenuItem(models.Model):
 	name = models.CharField(max_length=200)
 	quality_check = models.CharField(max_length=1000)
 
+	prep_yield = models.CharField(max_length=50)
+	shelf_life = models.CharField(max_length=50)
+
 	category = models.ForeignKey(Category)
 
 	def __unicode__(self):
@@ -23,3 +26,19 @@ class MenuItem(models.Model):
 	@classmethod
 	def get_items(cls):
 		return cls.objects.all().order_by('name')
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length=200)
+
+	menuItems = models.ManyToManyField(MenuItem, through="MenuIngredient")
+
+	def __unicode__(self):
+		return self.name
+
+class MenuIngredient(models.Model):
+	menuItem = models.ForeignKey(MenuItem)
+	ingredient = models.ForeignKey(Ingredient)
+
+	# The measurements stored can be pretty messed up, so keep em as text
+	amount_imperial = models.CharField(max_length=50)
+	amount_metric = models.CharField(max_length=50)
