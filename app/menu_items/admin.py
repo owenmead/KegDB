62c0c4ca3ff012368c4a1fedfef77a346701a2d5
owen <owenmead@gmail.com>
2009-12-31 13:49:51 -0800

    Update menu_item to add inline ingredient, as well as change extra fields

--- app/menu_items/admin.py
+++ app/menu_items/admin.py
@@ -2,25 +2,31 @@ from app.menu_items.models import Category, MenuItem, Ingredient, MenuIngredient
 from django.contrib import admin
 
 # === Menu Item Admin =========================================================
+class IngredientInline(admin.TabularInline):
+    model = MenuIngredient
+    extra = 6
+
 class MenuStorageStepInline(admin.TabularInline):
 	model = MenuStorageStep
-	extra = 1
+	extra = 3
 
 class MenuPrepStepInline(admin.TabularInline):
 	model = MenuPrepStep
-	extra = 1
+	extra = 6
 
 class MenuCookStepInline(admin.TabularInline):
 	model = MenuCookStep
-	extra = 1
+	extra = 3
 
 class MenuPresentationStepInline(admin.TabularInline):
 	model = MenuPresentationStep
-	extra = 1
+	extra = 3
 
 class MenuItemAdmin(admin.ModelAdmin):
-	inlines = [MenuPrepStepInline, MenuStorageStepInline, MenuCookStepInline, MenuPresentationStepInline]
+	search_fields = ['name']
+	inlines = [IngredientInline, MenuPrepStepInline, MenuStorageStepInline, MenuCookStepInline, MenuPresentationStepInline]
 	ordering = ('name',)
+	save_on_top = True
 
 # === Category Admin ==========================================================
 class MenuItemInline(admin.TabularInline):
@@ -46,7 +52,7 @@ class AllergenAdmin(admin.ModelAdmin):
 
 class IngredientAdmin(admin.ModelAdmin):
     ordering = ('name',)
-    inlines = [AllergenIngredientInline]
+    inlines = [AllergenIngredientInline, IngredientInline]
 
 admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(Category, CategoryAdmin)
