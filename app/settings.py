6162f29e164e41f608274b2bf0140b9ceabe5983
owen <owenmead@gmail.com>
2010-05-06 12:36:36 -0700

    Now using Postgres DB as backend

--- app/settings.py
+++ app/settings.py
@@ -9,12 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/owen/work/KegDB/app/sqlite3.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'kegdb'                 # Or path to database file if using sqlite3.
+DATABASE_USER = 'kegdb'                 # Not used with sqlite3.
+DATABASE_PASSWORD = 'kegdb'             # Not used with sqlite3.
+DATABASE_HOST = ''                      # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''                      # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
