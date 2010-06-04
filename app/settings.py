8d768cd361a2fa5f4508dd41861fd30f0920389b
Owen Mead-Robins <owenmead@gmail.com>
2010-06-04 15:50:41 -0700

    Now using the same db credentials as production server

--- app/settings.py
+++ app/settings.py
@@ -10,9 +10,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'kegdb'                 # Or path to database file if using sqlite3.
-DATABASE_USER = 'kegdb'                 # Not used with sqlite3.
-DATABASE_PASSWORD = 'kegdb'             # Not used with sqlite3.
+DATABASE_NAME = 'owenmead_keg'          # Or path to database file if using sqlite3.
+DATABASE_USER = 'owenmead_keg'          # Not used with sqlite3.
+DATABASE_PASSWORD = 'r0v3rb0y'          # Not used with sqlite3.
 DATABASE_HOST = ''                      # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''                      # Set to empty string for default. Not used with sqlite3.
 
